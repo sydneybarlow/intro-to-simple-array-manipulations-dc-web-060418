b2377e0c3141db15f5ac973331fe0cb5d37b3de8
Sydney Barlow <sydneybarlow44@gmail.com>
2018-05-31 18:11:49 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,8 +26,8 @@ def using_concat(array, new_array)
   array.concat(new_array)
 end
 
-def using_insert(array, new_array)
-  array.insert(new_array)
+def using_insert(array, new_element)
+  array.insert(new_element)
 end
 
 def using_uniq(array)
