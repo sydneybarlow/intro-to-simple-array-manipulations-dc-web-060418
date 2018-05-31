ce98fbc2af06afe0de9a51374e20d7f437e44c74
Sydney Barlow <sydneybarlow44@gmail.com>
2018-05-31 17:42:09 +0000

    automatically backed up by learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,47 @@
+def using_push(countries_in_western_africa, next_country)
+  countries_in_western_africa.push(next_country)
+end  
+
+def using_unshift(neighborhoods_in_northwest_brooklyn, @new_neighborhood)
+  neighborhoods_in_northwest_brooklyn.unshift(@new_neighborhood)
+end
+
+#def using_pop(array)
+#  array.pop
+#end
+
+#def pop_with_args(array)
+#  array.pop(2)
+#end
+
+#def using_shift(array)
+#  array.shift
+#end
+
+#def shift_with_args(array)
+#  array.shift(2)
+#end
+
+#def using_concat(array, new_array)
+#  array.concat(new_array)
+#end
+
+#def using_insert(array, new_array)
+#  array.insert(new_array)
+#end
+
+#def using_uniq(array)
+#  array.uniq()
+#end
+
+#def using_flatten(array)
+#  array.flatten()
+#end 
+
+#def using_delete(array, "string")
+#  array.delete("string")  
+#end
+
+#def using_delete_at(array, [x])
+#  array.delete([x]) 
+#end   
