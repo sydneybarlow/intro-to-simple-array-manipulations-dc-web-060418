1dc6ef05e2a15a5630cd0f0a641826f3e05e4930
Sydney Barlow <sydneybarlow44@gmail.com>
2018-05-31 17:51:04 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -18,9 +18,9 @@ def using_shift(array)
   array.shift
 end
 
-#def shift_with_args(array)
-#  array.shift(2)
-#end
+def shift_with_args(array)
+  array.shift(2)
+end
 
 #def using_concat(array, new_array)
 #  array.concat(new_array)
